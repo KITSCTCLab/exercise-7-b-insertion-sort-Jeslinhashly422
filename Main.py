41d285a8463eddaef0889fdfa71d4af3916e01b8
Jeslinhashly422 <109952248+Jeslinhashly422@users.noreply.github.com>
2022-09-12 15:32:18 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,14 @@
 from typing import List
-
 def insertionSort(array) -> List[int]:
-  # Write your code here
+  for step in range(1, len(array)):
+    key = array[step]
+    j = step - 1
+    while j >= 0 and key < array[j]:
+      array[j + 1] = array[j]
+      j = j - 1
+    array[j + 1] = key
+  return array
 
-# data = [9, 5, 1, 4, 3]
 input_data = input()
 data = []
 for item in input_data.split(', '):
@@ -11,4 +16,4 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(insertionSort(data))
+print(insertionSort(data)
